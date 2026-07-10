1fb3e308ebf7069337161a61c61fe368863fdf84
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-07-11 00:22:22 +1200

    player code

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,9 +1,23 @@
 extends CharacterBody3D
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 5.0
-@export var GRAVITY = 10.0
+@export var SPEED = 1.0
+@export var JUMP_VELOCITY = 3.0
+@export var GRAVITY = 9.76
+var min_pitch: float = deg_to_rad(-80)
+var max_pitch: float = deg_to_rad(80)
+var mouse_sensitivity = 0.002
 
+# Don't change
+var yaw: float = 0.0
+var pitch: float = 0.0
+
+
+
+#mouse capture
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+#Movement
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= 10 * delta
@@ -22,28 +36,16 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0.0, SPEED)
 
 	move_and_slide()
-	#var mouse_pos = get_viewport().get_mouse_position()
-	#
-	#var camera = $Camera3D
-#
-	#var ray_origin = camera.project_ray_origin(mouse_pos)
-	#var ray_direction = camera.project_ray_normal(mouse_pos)
-	#var plane = Plane(Vector3.UP, 0)
-	#var intersection = plane.intersects_ray(ray_origin, ray_direction)
-	#if intersection:
-		#print("Mouse position in 3D:", intersection)
-	var vp_size = get_viewport().get_visible_rect().size
-	var mouse_pos = get_viewport().get_mouse_position()
-	var left_size = (vp_size.x / 3)
-	var right_size = (vp_size.x * 2/3)
-	var top_size = (vp_size.y / 3)
-	var bottom_size = (vp_size.y * 2 / 3)
-	if mouse_pos.y < top_size:
-		$Camera3D.rotate_x(deg_to_rad(1))
-	if mouse_pos.y > bottom_size:
-		$Camera3D.rotate_x(deg_to_rad(-1))
-	
-	if mouse_pos.x < right_size:
-		rotate_y(deg_to_rad(1))
-	if mouse_pos.x > left_size:
-		rotate_y(deg_to_rad(-1))
+
+#See if mouse move and change where look
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * mouse_sensitivity
+		pitch -= event.relative.y * mouse_sensitivity
+		pitch = clamp(pitch, min_pitch, max_pitch)
+		rotation = Vector3(pitch, yaw, 0)
+
+#STop mouse capture
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
